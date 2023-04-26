d69290f2df20c1d600067017be8ed2f331d2d9d2
HuanWang <1796899275@qq.com>
2023-04-26 15:49:57 +0800

    wh4.26

--- ams-gateway/src/main/java/com/scu/ams/gateway/AmsGatewayApplication.java
+++ ams-gateway/src/main/java/com/scu/ams/gateway/AmsGatewayApplication.java
@@ -2,10 +2,13 @@ package com.scu.ams.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import javax.sql.DataSource;
+
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class})
 public class AmsGatewayApplication {
 
     public static void main(String[] args) {
